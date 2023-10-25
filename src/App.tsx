34552d57487aebdd2363b43d4849fbe964f7aa42
MJAmador <majalo.365@gmail.com>
2023-10-25 12:47:22 -0600

    first commit rqm

--- src/App.tsx
+++ src/App.tsx
@@ -37,7 +37,7 @@ function App() {
         <div className="quote-content" style={{ color: randomColor, transition }}> 
           <h2 id="text">
             <FaQuoteLeft size="30" style={{ marginRight: "10px" }} />
-            { quote.quote }
+            {quote.text}
             <FaQuoteRight size="30" style={{ marginLeft: "10px" }} />
             </h2>
           <h4 id="author">- {quote.author}</h4>
